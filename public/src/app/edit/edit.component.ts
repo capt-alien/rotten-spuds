db335b384c371df07dde4ada39c40c0337b6d60f
capt-alien <eric.botcher@presidio.edu>
2019-10-23 13:32:49 -0700

    something

--- public/src/app/edit/edit.component.ts
+++ public/src/app/edit/edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  movie={
+    title:'',
+    year: '',
+    summery:''
+  }
+  errors;
 
-  constructor() { }
+  constructor(
+              private route: ActivatedRoute,
+              private http: HttpService,
+              private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((data:any)=>{
+      this.getOnebyID(data.id)
+    })
   }
+  getOnebyID(id){
+    const observable = this.http.updateMovie(id, this.movie);
+    observable.subscribe((data:any)=>{
+      console.log(data);
+      this.movie = data
+      }
+    )
+  }
+  buttonSubmit(id){
+    console.log(this.movie)
+    const observable = this.http.updateMovie(id, this.movie);
+    observable.subscribe((data:any)=>{
+      if(data.message==='fail'){
+        this.errors = data.error['message'];
+      }else{
+        console.log("edit made");
+        this.router.navigate(['/'])
+    }
+  })
+}
 
 }
